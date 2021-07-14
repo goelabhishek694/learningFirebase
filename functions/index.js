13e595fa953205b8feb473bec385638c0b273a40
goelabhishek694 <goelabhishek694@gmail.com>
2021-07-14 23:14:43 +0530

    updated app

--- functions/index.js
+++ functions/index.js
@@ -1,71 +1,71 @@
-// const { firebaseConfig } = require("firebase-functions");
-// const functions = require("firebase-functions");
-// const admin=require("firebase-admin");
-// admin.initializeApp();
+const { firebaseConfig } = require("firebase-functions");
+const functions = require("firebase-functions");
+const admin=require("firebase-admin");
+admin.initializeApp();
 
 
-// // // Create and Deploy Your First Cloud Functions
-// // // https://firebase.google.com/docs/functions/write-firebase-functions
-// //
-// // exports.helloWorld = functions.https.onRequest((request, response) => {
-// //   functions.logger.info("Hello logs!", {structuredData: true});
-// //   response.send("Hello from Firebase!");
-// // });
-// // http req 1 eslint error for indentation
-// // exports.randomNumber = functions.https.onRequest((req, res) => {
-// //   const number = Math.round(Math.random() * 100);
-// //   res.send(number.toString());
-// // });
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+// http req 1 eslint error for indentation
+// exports.randomNumber = functions.https.onRequest((req, res) => {
+//   const number = Math.round(Math.random() * 100);
+//   res.send(number.toString());
+// });
 
-// // // http req 2
-// // exports.toAnotherWebsite=functions.https.onRequest((req, res)=>{
-// //   res.redirect("https://www.zunroof.com/");
-// // });
+// // http req 2
+// exports.toAnotherWebsite=functions.https.onRequest((req, res)=>{
+//   res.redirect("https://www.zunroof.com/");
+// });
 
-// // // http callable function
-// // exports.sayHello=functions.https.onCall((data, context)=>{
-// //     const name=data.name;
-// //     const auth=context;
-// //     // eslint-disable-next-line quotes
-// //     return `hello ${name} how are ${auth}`;
-// // });
+// // http callable function
+// exports.sayHello=functions.https.onCall((data, context)=>{
+//     const name=data.name;
+//     const auth=context;
+//     // eslint-disable-next-line quotes
+//     return `hello ${name} how are ${auth}`;
+// });
 
 
-// // auth trigger (new user sign up)
-// exports.newUserSignUp=functions.auth.user().onCreate((user)=>{
-// //   console.log("user created", user.email, user.uid);
-//   // for background triggers you must return a value/promise
-// //   firebase
-//   return admin.firestore().collection("users").doc(user.uid).set({
-//     email: user.email,
-//     upVotedOn: []}
+// auth trigger (new user sign up)
+exports.newUserSignUp=functions.auth.user().onCreate((user)=>{
+//   console.log("user created", user.email, user.uid);
+  // for background triggers you must return a value/promise
+//   firebase
+  return admin.firestore().collection("users").doc(user.uid).set({
+    email: user.email,
+    upVotedOn: []}
+  );
+});
+
+// auth trigger (user deleted)
+exports.deleteUser=functions.auth.user().onDelete((user)=>{
+// console.log("user deleted", user.email, user.uid);
+  // for background triggers you must return a value/promise
+  const doc=admin.firestore().collection("users").doc(user.uid);
+  return doc.delete();
+});
+
+exports.addRequest=functions.https.onCall((data, context)=>{
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+        "unauthenticated",
+        "only authenticated users can add requests"
+    );
+  }
+  if (data.text.length>30) {
+    throw new functions.https.HttpsError(
+        "invalid-argument",
+        "request must be less than 30 characters"
+    );
+  }
+//   admin.firestore().collection("requests").add({
+//     request: data.text,
+//     upvotes: 0}
 //   );
 // });
-
-// // auth trigger (user deleted)
-// exports.deleteUser=functions.auth.user().onDelete((user)=>{
-// // console.log("user deleted", user.email, user.uid);
-//   // for background triggers you must return a value/promise
-//   const doc=admin.firestore().collection("users").doc(user.uid);
-//   return doc.delete();
-// });
-
-// exports.addRequest=functions.https.onCall((data, context)=>{
-//   if (!context.auth) {
-//     throw new functions.https.HttpsError(
-//         "unauthenticated",
-//         "only authenticated users can add requests"
-//     );
-//   }
-//   if (data.text.length>30) {
-//     throw new functions.https.HttpsError(
-//         "invalid-argument",
-//         "request must be less than 30 characters"
-//     );
-//   }
-// //   admin.firestore().collection("requests").add({
-// //     request: data.text,
-// //     upvotes: 0}
-// //   );
-// // });
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,26 @@
-const requestModal = document.querySelector('.new-request');
-const requestLink = document.querySelector('.add-request');
-const btnClick=document.querySelector('.click');
+// const requestModal = document.querySelector('.new-request');
+// const requestLink = document.querySelector('.add-request');
+// const btnClick=document.querySelector('.click');
 
-// open request modal
-requestLink.addEventListener('click', () => {
-  requestModal.classList.add('open');
-});
-
-// close request modal
-requestModal.addEventListener('click', (e) => {
-  if (e.target.classList.contains('new-request')) {
-    requestModal.classList.remove('open');
-  }
-});
-
-// btnClick.addEventListener("click",function(){
-//   const sayHello=firebase.functions().httpsCallable('sayHello');
-//   sayHello({'name': 'Abhishek' },"123434").then(result=>{
-//     // console.log(result);
-//     console.log(result.data);
-//   });
+// // open request modal
+// requestLink.addEventListener('click', () => {
+//   requestModal.classList.add('open');
 // });
+
+// // close request modal
+// requestModal.addEventListener('click', (e) => {
+//   if (e.target.classList.contains('new-request')) {
+//     requestModal.classList.remove('open');
+//   }
+// });
+
+// // btnClick.addEventListener("click",function(){
+// //   const sayHello=firebase.functions().httpsCallable('sayHello');
+// //   sayHello({'name': 'Abhishek' },"123434").then(result=>{
+// //     // console.log(result);
+// //     console.log(result.data);
+// //   });
+// // });
 
 
 
